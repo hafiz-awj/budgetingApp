c7286a45c8f9bce4bd8cd79103a0766874ba2162
ali-awj <hafizaliawj@gmail.com>
2023-06-03 22:35:42 +0500

    commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let myExpenses = [];
 const afterEnteringName = () => {
   let name = document.getElementById("name");
   if (name.value !== "") {
@@ -16,7 +17,7 @@ const afterEnteringBudget = () => {
   let name = document.getElementById("name");
   let budget = document.getElementById("monthly-budget");
   let remainingBudget = document.getElementById("div3-remaining-budget");
-  let totalExpenses = document.getElementById("div3-total-expenses");
+let totalExpenses = document.getElementById("div3-total-expenses");
   if (budget.value > 0) {
     let div1 = document.getElementById("div1");
     div1.style.display = "none";
@@ -27,6 +28,8 @@ const afterEnteringBudget = () => {
     let hiName = document.getElementById("div3-name");
     hiName.innerHTML = `Hi <br>"<strong> ${name.value} </strong>"`;
     let yourBudget = document.getElementById("div3-budget");
+    remainingBudget.value = budget.value;
+    totalExpenses.value = 0;
     yourBudget.innerHTML = `Your total budget for this month is <br>" <strong> ${budget.value} </strong>"`;
     remainingBudget.innerHTML = `Your remaining budget for this month is <br> "<strong> ${remainingBudget.value} </strong>"`;
     totalExpenses.innerHTML = `Your total expenses reach to <br> "<strong>${totalExpenses.value}</strong>"`;
@@ -46,8 +49,6 @@ const expenseButton = () => {
 };
 
 // Holding Expenses
-
-let myExpenses = [];
 const addExpense = () => {
   let category = document.getElementById("category").value;
   let amount = document.getElementById("amount").value;
@@ -68,6 +69,8 @@ const addExpense = () => {
         description,
       };
       myExpenses.push(expenses);
+      createExpenses();
+      result();
     } else if (category === "") {
       alert("Select a valid category");
     } else if (amount <= 0) {
@@ -76,4 +79,48 @@ const addExpense = () => {
       alert("Enter valid description");
     }
   }
+};
+const createExpenses = () => {
+  const expenseElement = document.querySelector("main");
+  expenseElement.innerHTML = "";
+  myExpenses.forEach((expense, index) => {
+    const expenseDiv = document.createElement("div");
+    expenseDiv.innerHTML = `
+    <div class="div3-expense-serial all-expenses"><h2><pre>   ${index + 1}</pre></h2><p>${expense.date}</p></div>
+    <div class="all-expenses div3-expense-body">
+      <h3><pre>${expense.category}                                      Rs. ${expense.amount}</pre> </h3>
+      <p>${expense.description}</p>
+    </div>
+    <button onclick="deleteExpense(${index})" class="all-expenses div3-expense-remove">Remove Expense</button>`;
+    expenseDiv.id = "div3-expenses"
+    expenseElement.appendChild(expenseDiv);
+    expenseElement.style.marginTop = "150px";
+  });
+};
+
+const deleteExpense = index => {
+  myExpenses.splice(index,1);
+  createExpenses();
+  result();
+}
+
+const calculations = () => {
+  const budget = document.getElementById("monthly-budget").value;
+  let totalExpenses = 0;
+  let remainingBudget = budget;
+  myExpenses.forEach(expense => {
+    totalExpenses += Number(expense.amount);
+  });
+  remainingBudget = remainingBudget - totalExpenses;
+  return [totalExpenses, remainingBudget];
+};
+
+const result = () => {
+  const [totalExpenses, remainingBudget] = calculations();
+  const div3TotalExpenses = document.getElementById("div3-total-expenses");
+  const div3RemainingBudget = document.getElementById("div3-remaining-budget");
+  div3TotalExpenses.innerHTML = 
+  `Your total expenses reach to <br><strong>${totalExpenses}</strong>`;
+  div3RemainingBudget.innerHTML = 
+  `Your remaining budget <br><strong>${remainingBudget}</strong>`;
 };
